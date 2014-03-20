735b24a257d5b6ae5a0dbadfcfa9cae40a976e06
Jakub Szafrański <s@samu.pl>
2014-03-20 21:03:36 +0100

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ django-admin-autoregister
 =========================
 
 Automatically register all your models in your django admn site.
+
+Description
+-----------
+
+Using this django application, you don't have to manually register all your models in the admin site. 
+All your models will be automatically registered, with a dynamically generated ModelAdmin class, that will format all your models nicely.
+
+If you're just using the admin site to edit your models internally (if the only thing that you care about is a convenient way to edit your database), then this app should do it for you.
+
+Usage
+-----
+
+All you have to do, is to copy the application into your app root, and append it into the *INSTALLED_APPS* settings variable:
+
+```python
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    # ...
+    'django_autoregister',
+)
+```
+
+After your project restarts (either by hand, or automatically, if you're using the developement server), you should see all your models and applications registered in your admin site.
---
+++ django_autoregister/admin.py
@@ -0,0 +1,59 @@
+from django.contrib import admin
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import get_models, get_app
+from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered
+from django.conf import settings
+
+
+class AutoDisplayAdmin(admin.ModelAdmin):
+    list_links_fields = ('CharField',
+                         'IntegerField',
+                         'AutoField',
+                         'DateField',
+                         'DateTimeField',
+                         'SlugField',
+                         'BigIntegerField',
+                         'EmailField',
+                         'BooleanField',
+                         'DecimalField',
+                         'FloatField',
+                         'IPAddressField',
+                         'GenericIPAddressField',
+                         'NullBooleanField',
+                         'PositiveIntegerField',
+                         'PositiveSmallIntegerField',
+                         'UrlField',
+                         'TimeField',)
+    list_display_fields = list_links_fields + ('ForeignKey', )
+
+    def __init__(self, *args, **kwargs):
+        admin.ModelAdmin.__init__(self, *args, **kwargs)
+        self.list_display = []
+        self.list_display_links = []
+        print args
+        for field in args[0]._meta.fields:
+            if field.get_internal_type() in self.list_display_fields:
+                self.list_display.append(field.name)
+            if len(self.list_display_links) < 2 and field.get_internal_type() in self.list_links_fields:
+                self.list_display_links.append(field.name)
+
+
+
+def auto_register(*app_list):
+    for app_name in app_list:
+        try:
+            app_models = get_app(app_name)
+        except ImproperlyConfigured:
+            print "Application {app_name} could not be automatically registered to admin, as it could not be found. " \
+                  "Skipping...".format(app_name=app_name)
+            continue
+        for model in get_models(app_models):
+            try:
+                admin.site.register(model, AutoDisplayAdmin)
+            except AlreadyRegistered:
+                pass
+
+
+app_list = [app for app in settings.INSTALLED_APPS]
+auto_register(*app_list)
